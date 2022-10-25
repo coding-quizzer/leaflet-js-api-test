ab20bac15949cd2759029146b0ba832ccf26a574
William Gadd <wgadd@sasktel.net>
2022-10-25 01:21:50 +0000

    Make a server file to serve map-test.html

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const bodyParser = require("body-parser");
+
 
 const PORT = 8080;
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.redirect('/map-test.html')
+});
+
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);

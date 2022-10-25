fefde5dd59f18316eaf98a5e4e7a021cd9ac4920
William Gadd <wgadd@sasktel.net>
2022-10-25 00:37:30 +0000

    Add popup to marker on map

--- index.js
+++ index.js
@@ -5,4 +5,7 @@ $(() => {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
   }).addTo(map);
   var marker = L.marker([50.482456, -104.615591]).addTo(map);
+  marker.bindPopup('<p>Here is a popup</p>').openPopup();
+  const $marker = $(marker);
+  console.log(marker);
 });
